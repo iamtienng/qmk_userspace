7fb7ca12eb6cd7eb776ec21ec550dc73711e7ea9
Tien M. Nguyen <iamtienng@gmail.com>
2025-03-21 18:51:29 +0100

    Update keymap.c

--- keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vendor/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_LSFT,    PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_RSFT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                    KC_LGUI, KC_SPC,  LL_TAB,     RL_ENT,  KC_SPC,
-                                            KC_DEL,   LOWER,     KC_BSPC
+                                            KC_DEL,   LOWER,    KC_BSPC
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
